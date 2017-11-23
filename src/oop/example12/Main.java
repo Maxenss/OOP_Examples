31d6ed52691d241460c13bd79e22b4ce9f998a05
Maxenss <maxenssss@gmail.com>
2017-11-23 17:39:49 +0300

    For Oleg

---
+++ src/oop/example12/Main.java
@@ -0,0 +1,16 @@
+package oop.example12;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        BadPupil badPupil = new BadPupil();
+        GoodPupil goodPupil = new GoodPupil();
+        GoodPupil goodPupil12 = new GoodPupil();
+        ExcelentPupil excelentPupil = new ExcelentPupil();
+
+        ClassRoom classRoom =
+                new ClassRoom(badPupil, goodPupil, goodPupil12, excelentPupil);
+        classRoom.showPupilsInfo();
+    }
+}
